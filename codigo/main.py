3fdbe57618a98e7930b5facdf1b1aef08ecbdca6
Pedro Augusto <95659638+PedroAug92@users.noreply.github.com>
2023-01-25 16:44:29 -0300

    Correções no arquivo main.py
    Tratando erros relacionados a leitura de criação de arquivos e fazendo mudanças na UI do programa.
    
    Co-Authored-By: Pedro Pegado <93950768+PedroPegado@users.noreply.github.com>

--- codigo/main.py
+++ codigo/main.py
@@ -6,37 +6,53 @@ from professor import Professor
 def checaExiste(nomeArquivo):
     try:
         with open(nomeArquivo, 'r') as arquivo:
-            pass
+            if (nomeArquivo == "disciplinas.txt"):
+                dadosDisciplinas = arquivo.readlines()
+                return dadosDisciplinas
     except: 
         with open(nomeArquivo, 'w') as arquivo:
             pass
+    
 
-for c in ['alunos.txt', 'professores.txt', 'disciplinas.txt']:
-    checaExiste(c)
-
-with open('disciplinas.txt', 'r') as arquivo:
-    bancoDados = arquivo.readlines()
+for nome in ['alunos.txt', 'professores.txt', 'disciplinas.txt']:
+    bancoDados = checaExiste(nome)
 
 dicio = {}
 
-for linha in bancoDados:
-    dados = linha.split(':')
-    coddis = dados[0]
-    nomedisc = dados [1]
-    dicio[coddis] = nomedisc
-
-print(dicio)
+try:
+    for linha in bancoDados:
+        dados = linha.split(':')
+        coddis = dados[0]
+        nomedisc = dados [1]
+        dicio[coddis] = nomedisc
+    
+    print(dicio)
+except TypeError as error:
+    pass
 
 while True:
     print('''DIGITE A OPÇÃO DESEJADA:
     1 - CRIAR DISCIPLINA
-    2 - EMITIR RELATÓRIO DE DISCIPLINA''')
-    opc = int(input('> '))
-    if opc == 1:
-        ch = int(input('Carga horaria > '))
-        nome = str(input('Nome > '))
-        codDisci = input('#')
-        Disciplina(ch, nome, codDisci)
-    elif opc == 2:
-        opc2 = input('Digite o código da disciplina > ')
-        Disciplina.emitirRelatorio(dicio[opc2]) 
+    2 - EMITIR RELATÓRIO DE DISCIPLINA
+    3 - ENCERRAR O PROGRAMA''')
+
+    try:
+        acao = int(input('> '))
+    except:
+        print("Ação inválida.")
+    else:
+        if (acao == 1):
+            print("Carga horaria:")
+            ch = int(input("> "))
+            print("Nome da disciplina:")
+            nome = str(input("> "))
+            print("Código da disciplina:")
+            codDisci = input("> #")
+            Disciplina(ch, nome, codDisci)
+        elif (acao == 2):
+            print("Digite o código da disciplina:")
+            opc2 = input('#')
+            Disciplina.emitirRelatorio()
+        elif (acao == 3):
+            print("Até breve...")
+            break
