2ec0033bbca2c4e0e85565e4c40d0f9a09295713
Pedro Pegado <93950768+PedroPegado@users.noreply.github.com>
2023-01-25 12:01:51 -0300

    Tentando implementar Dicionários
    implementando dicionário no arquivo main.py como indicado pelo professor Gilbran, para resolver o problema do método emitirRelatorio() da classe Disciplina.
    
    Co-Authored-By: Pedro Augusto <95659638+PedroAug92@users.noreply.github.com>

--- codigo/main.py
+++ codigo/main.py
@@ -13,3 +13,30 @@ def checaExiste(nomeArquivo):
 
 for c in ['alunos.txt', 'professores.txt', 'disciplinas.txt']:
     checaExiste(c)
+
+with open('disciplinas.txt', 'r') as arquivo:
+    bancoDados = arquivo.readlines()
+
+dicio = {}
+
+for linha in bancoDados:
+    dados = linha.split(':')
+    coddis = dados[0]
+    nomedisc = dados [1]
+    dicio[coddis] = nomedisc
+
+print(dicio)
+
+while True:
+    print('''DIGITE A OPÇÃO DESEJADA:
+    1 - CRIAR DISCIPLINA
+    2 - EMITIR RELATÓRIO DE DISCIPLINA''')
+    opc = int(input('> '))
+    if opc == 1:
+        ch = int(input('Carga horaria > '))
+        nome = str(input('Nome > '))
+        codDisci = input('#')
+        Disciplina(ch, nome, codDisci)
+    elif opc == 2:
+        opc2 = input('Digite o código da disciplina > ')
+        Disciplina.emitirRelatorio(dicio[opc2]) 
