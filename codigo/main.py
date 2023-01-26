8bb696e1b50fe1f3a8c7b27f50014fd744adb860
Pedro Pegado <93950768+PedroPegado@users.noreply.github.com>
2023-01-26 14:43:51 -0300

    Update main.py
    Criando funções e tratando erros, como a criação de disciplinas com mesmo código.
    
    Co-Authored-By: Pedro Augusto <95659638+PedroAug92@users.noreply.github.com>

--- codigo/main.py
+++ codigo/main.py
@@ -3,7 +3,7 @@ from disciplina import Disciplina
 from professor import Professor
 
 
-def checaExiste(nomeArquivo):
+def checa_existe(nomeArquivo):
     try:
         with open(nomeArquivo, 'r') as arquivo:
             if (nomeArquivo == "disciplinas.txt"):
@@ -12,10 +12,46 @@ def checaExiste(nomeArquivo):
     except: 
         with open(nomeArquivo, 'w') as arquivo:
             pass
-    
+
+def estrutura_disciplina(dicionario, acao_usuario):
+    if (acao_usuario == 1):
+        print("Carga horaria:")
+        carga_horaria = int(input("> "))
+        print("Nome da disciplina:")
+        nome_disciplina = input("> ")
+        print("Código da disciplina:")
+        codigo_disciplina = input("> #")
+        if dicionario.get(f'#{codigo_disciplina}') == None:
+            disciplina = Disciplina(codigo_disciplina, nome_disciplina, carga_horaria)
+
+            linha = f"#{codigo_disciplina}:{nome_disciplina}:{carga_horaria}:\n"
+        else:
+            print('=' * 20)
+            print('Código já cadastrado')
+            print('=' * 20)
+            return 'volta'
+
+        with open("disciplinas.txt", 'a') as arquivo:
+            arquivo.write(linha)
+    elif (acao_usuario == 2):
+        print("Digite o código da disciplina:")
+        codigo_disciplina = input('#')
+        materia = dicionario.get(f'#{codigo_disciplina}')
+        if (materia == None):
+            print('Disciplina inexistente')
+            return 'volta'
+        else:
+            nome_disciplina = materia[0]
+            carga_horaria = materia[1]
+            disciplina = Disciplina(codigo_disciplina, nome_disciplina, carga_horaria)
+            disciplina.emitirRelatorio()
+    elif (acao_usuario == 3):
+        print("Até breve...")
+        return 'sair'
+
 
 for nome in ['alunos.txt', 'professores.txt', 'disciplinas.txt']:
-    dados_disciplina = checaExiste(nome)
+    dados_disciplina = checa_existe(nome)
 
 disciplinas_dic = {}
 
@@ -29,44 +65,25 @@ except TypeError as error:
 
 while True:
     print('''\nDIGITE A OPÇÃO DESEJADA:
-    1 - CRIAR DISCIPLINA
-    2 - EMITIR RELATÓRIO DE DISCIPLINA
-    3 - ENCERRAR O PROGRAMA''')
+    1 - DISCIPLINA
+    2 - PROFESSOR
+    3 - ALUNO''')
 
     try:
         acao_usuario = int(input('> '))
     except:
         print("Ação inválida.")
     else:
-        if (acao_usuario == 1):
-            print("Carga horaria:")
-            carga_horaria = int(input("> "))
-            print("Nome da disciplina:")
-            nome_disciplina = input("> ")
-            print("Código da disciplina:")
-            codigo_disciplina = input("> #")
-
-            disciplina = Disciplina(codigo_disciplina, nome_disciplina, carga_horaria)
-
-            linha = f"#{codigo_disciplina}:{nome_disciplina}:{carga_horaria}:\n"
-
-            with open("disciplinas.txt", 'a') as arquivo:
-                arquivo.write(linha)
-        elif (acao_usuario == 2):
-            print("Digite o código da disciplina:")
-            codigo_disciplina = input('#')
-            materia = disciplinas_dic.get(f'#{codigo_disciplina}')
-            if (materia == None):
-                print('Matéria inexistente')
+        if acao_usuario == 1:
+            retorno = estrutura_disciplina(disciplinas_dic, acao_usuario)
+            if retorno == 'sair':
+                break
+            elif retorno == 'volta':
                 continue
-            else:
-                nome_disciplina = materia[0]
-                carga_horaria = materia[1]
-                disciplina = Disciplina(codigo_disciplina, nome_disciplina, carga_horaria)
-                disciplina.emitirRelatorio()
-        elif (acao_usuario == 3):
-            print("Até breve...")
-            break
+        elif acao_usuario == 2:
+            pass
+        elif acao_usuario == 3:
+            pass
 
 """
 TO-DOs
