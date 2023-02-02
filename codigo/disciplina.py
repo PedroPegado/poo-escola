4977cb4989d41b4a816990691b53683fda245acb
Pedro Augusto <95659638+PedroAug92@users.noreply.github.com>
2023-02-02 15:14:39 -0300

    Att classe disciplina
    Checando se uma disciplina tem um professor.

--- codigo/disciplina.py
+++ codigo/disciplina.py
@@ -2,8 +2,18 @@ class Disciplina:
     def __init__(self, codigoDisciplina, nome, cargaHoraria):
         self.cargaHoraria = cargaHoraria
         self.nome = nome
-        self.professor = ""
+        self.professor = None
         self.codigoDisciplina = codigoDisciplina
+
+        with open("professores.txt", "r") as arquivo:
+            linhas = arquivo.readlines()
+
+        for linha in linhas:
+            dados = linha.split(":")
+            disciplinas_professor = dados[2]
+            if (f"#{codigoDisciplina}" in disciplinas_professor):
+                self.professor = dados[1]
+                break
         
     
     def emitirRelatorio(self):
@@ -11,7 +21,7 @@ class Disciplina:
         print("Nome da disciplina:", self.nome)
         print("Carga Horária da disciplina:", self.cargaHoraria)
         print(f"Código da disciplina: #{self.codigoDisciplina}")
-        if (self.professor == ""):
+        if (self.professor == None):
             print("Professor: Indeterminado")
         else:
             print("Professor:", self.professor)
